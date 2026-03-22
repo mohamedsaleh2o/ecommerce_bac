15d278adf65d1385180f700771215fd3b6644782
saleh <mohamedsalehofficial@gmail.com>
2026-03-22 03:05:47 +0200

    refactor: enhance product controller to validate product ID before operations

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,14 +1,18 @@
 const productService = require('../services/product.service');
 const catchAsync = require('../utils/catchAsync');
 const sendResponse = require('../utils/response');
+const { AppError } = require('../middlewares/error.middleware');
 
 const getProducts = catchAsync(async (req, res) => {
   const result = await productService.getProducts(req.query);
   sendResponse(res, 200, result.products, 'Products fetched successfully');
 });
 
-const getProductById = catchAsync(async (req, res) => {
-  const product = await productService.getProductById(Number(req.params.id));
+const getProductById = catchAsync(async (req, res, next) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) return next(new AppError('Invalid product ID', 400));
+  
+  const product = await productService.getProductById(id);
   sendResponse(res, 200, product, 'Product fetched successfully');
 });
 
@@ -17,13 +21,19 @@ const createProduct = catchAsync(async (req, res) => {
   sendResponse(res, 201, product, 'Product created successfully');
 });
 
-const updateProduct = catchAsync(async (req, res) => {
-  const product = await productService.updateProduct(Number(req.params.id), req.body);
+const updateProduct = catchAsync(async (req, res, next) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) return next(new AppError('Invalid product ID', 400));
+
+  const product = await productService.updateProduct(id, req.body);
   sendResponse(res, 200, product, 'Product updated successfully');
 });
 
-const deleteProduct = catchAsync(async (req, res) => {
-  await productService.deleteProduct(Number(req.params.id));
+const deleteProduct = catchAsync(async (req, res, next) => {
+  const id = Number(req.params.id);
+  if (isNaN(id)) return next(new AppError('Invalid product ID', 400));
+
+  await productService.deleteProduct(id);
   sendResponse(res, 200, {}, 'Product deleted successfully');
 });
 
